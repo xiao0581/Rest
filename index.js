3693ca0c5813d2a9bcde927b69909eb8ce434454
xiaohui <xiaohui@XIAO-PC>
2024-04-17 11:36:46 +0200

    change

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ Vue.createApp({
             Title: null,
             price: null,
             message: null,
-            posts: [],
+            books: [],
             singlePoster: null,
             enrro: null,
             getId: 0,
             addData: { title: "", price: 0 },
-            addMessage: null
+            addMessage: null,
+            updateBook: {id: null, title: "",price: null},
+            deleteMessage: null
         }
     },
     methods: {
@@ -18,12 +20,12 @@ Vue.createApp({
         async getAll() {
             try {
                 const res = await axios.get(baseUrl)
-                this.posts = await res.data
+                this.books = await res.data
             } catch (e) {
                 this.error = e
             }
         },
-        
+
         async getById(id) {
             const url = baseUrl + "/" + id
             try {
@@ -40,7 +42,26 @@ Vue.createApp({
             try {
                 response = await axios.post(baseUrl, this.addData)
                 this.addMessage = "response " + response.status + " " + response.statusText
-                
+                this.getAll()
+            } catch (ex) {
+                alert(ex.message)
+            }
+        },
+        async deleteBook(id) {
+            const url = baseUrl + "/" + id
+            try {
+                response = await axios.delete(url)
+                this.getAll()
+            } catch (ex) {
+                alert(ex.message)
+            }
+        },
+        async update() {
+            const url = baseUrl + "/" + this.updateBook.id
+            try {
+                response = await axios.put(url, this.updateBook)
+                this.addMessage = "response " + response.status + " " + response.statusText
+                this.getAll()
             } catch (ex) {
                 alert(ex.message)
             }
